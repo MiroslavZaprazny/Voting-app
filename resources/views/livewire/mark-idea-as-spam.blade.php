50c8eebc3f47587ac328cffa3c81788db06684fb
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-07-23 21:11:54 +0200

    Refactor modals to component

--- resources/views/livewire/mark-idea-as-spam.blade.php
+++ resources/views/livewire/mark-idea-as-spam.blade.php
@@ -1,53 +1,10 @@
-<div 
-    x-cloak
-    x-data ="{ isOpen:false }"
-    x-show="isOpen"
-    @keydown.escape.window="isOpen = false"
-    @custom-show-spam-modal.window="isOpen = true"
-    x-init="
-    window.livewire.on('ideaWasMarkedAsSpam', () => {
-        isOpen = false
-    })"
-    class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div 
-        x-show="isOpen"
-        x-transition.opacity
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-    <div class="fixed z-10 inset-0 overflow-y-auto">
-        <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-            <div         
-                x-show="isOpen"
-                x-transition.origin.bottom.duration.300ms
-                class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div class="sm:flex sm:items-start">
-                        <div
-                            class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <!-- Heroicon name: outline/exclamation -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
-                              </svg>
-                        </div>
-                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Mark this idea as spam
-                            </h3>
-                            <div class="mt-2">
-                                <p class="text-sm text-gray-500">Are your sure you want to mark this idea as spam? This action
-                                    cannot be undone.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button
-                        wire:click="markAsSpam" type="button"
-                        class="w-full bg-red inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Yes</button>
-                    <button 
-                        @click="isOpen = false"
-                        type="button"
-                        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Take me back</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<x-modal-confirm 
+    eventToOpenModal="custom-show-spam-modal"
+    eventToCloseModal="ideaWasMarkedAsSpam"
+    modalTitle="Mark this idea as spam"
+    modalDescription="Are your sure you want to mark this idea as spam? This action
+    cannot be undone."
+    modalConfirmBtnText="Yes"
+    modalBackBtnText="Take me back"
+    wireClick="markAsSpam"
+/>
